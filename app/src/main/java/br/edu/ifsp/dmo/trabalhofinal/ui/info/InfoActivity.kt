fe498f97115dc5e6024590b10b4ea759981afc74
Guilherme Crespo AQ3022889 <guilherme.crespo@aluno.ifsp.edu.br>
2025-02-10 02:55:27 -0300

    Adicionado o layout da InfoActivity, e configuração de listeners

--- app/src/main/java/br/edu/ifsp/dmo/trabalhofinal/ui/info/InfoActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo/trabalhofinal/ui/info/InfoActivity.kt
@@ -1,21 +1,45 @@
 package br.edu.ifsp.dmo.trabalhofinal.ui.info
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import br.edu.ifsp.dmo.trabalhofinal.R
+import br.edu.ifsp.dmo.trabalhofinal.databinding.ActivityInfoBinding
+import br.edu.ifsp.dmo.trabalhofinal.ui.main.MainActivity
 
 class InfoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityInfoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_info)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val txtLink1 = binding.infoTxtLink1
+        val txtlink2 = binding.infoTxtLink2
+        val btnVoltar = binding.buttonBack
+
+        txtLink1.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW,Uri.parse("https://www.sementesflorestais.org/mapa-das-sementes.html"))
+            startActivity(intent)
         }
+
+        txtlink2.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW,Uri.parse("https://arborescer.com.br/"))
+            startActivity(intent)
+        }
+
+        btnVoltar.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
+
     }
+
+
 }
